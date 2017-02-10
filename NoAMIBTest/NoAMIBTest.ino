ba33df6979585ae2d6632d01e69f27a492be0466
Liam Searson <lsearson123@gmail.com>
2017-02-10 12:18:40 -0800

    Add files via upload

---
+++ NoAMIBTest/NoAMIBTest.ino
@@ -0,0 +1,29 @@
+#include <FlexyStepper.h>
+
+const byte stepPinNumber = 34;
+const byte directionPinNumber = 35;
+const byte enablePinNumber = 33;
+
+FlexyStepper stepper;
+
+void setup() {
+  stepper.connectToPins(stepPinNumber, directionPinNumber, enablePinNumber);
+  
+  stepper.setSpeedInStepsPerSecond(100);
+  stepper.setAccelerationInStepsPerSecondPerSecond(100);
+  
+  stepper.enableStepper();
+}
+
+void loop() {
+  stepper.setTargetPositionRelativeInSteps(100);
+  while(!stepper.motionComplete()){
+    stepper.processMovement();
+  }
+  
+  stepper.setTargetPositionRelativeInSteps(-100);
+  while(!stepper.motionComplete()){
+    stepper.processMovement();
+  }
+
+}
